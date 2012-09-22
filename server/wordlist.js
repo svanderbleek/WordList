6316e48cfdeed0f73aff5f2df5dac7d0b9500a98
Sandy Vanderbleek <sandy.vanderbleek@gmail.com>
2012-09-21 18:13:22 -0700

    deployable dummy server

---
+++ server/wordlist.js
@@ -0,0 +1,11 @@
+var flatiron = require('flatiron'),
+  app = flatiron.app;
+
+app.use(flatiron.plugins.http);
+
+app.router.get('/', function () {
+  this.res.writeHead(200, { 'Content-Type': 'text/plain' });
+  this.res.end('up');
+});
+
+app.start(8080);
