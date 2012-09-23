ace755a82e4cf72ee59cc7efff0908d29ba7cdb4
Sandy Vanderbleek <sandy.vanderbleek@gmail.com>
2012-09-22 21:29:18 -0700

    begin sketching api for GET lists/name

--- server/server.js
+++ server/server.js
@@ -4,13 +4,18 @@ var flatiron = require('flatiron'),
 app.use(flatiron.plugins.http);
 
 app.router.get('/', function () {
-  this.res.writeHead(200, { 'Content-Type': 'text/plain' });
-  this.res.end('up');
+  this.res.writeHead(200, { 'Content-Type': 'application/json' });
+  this.res.end();
 });
 
 app.router.get('/lists', function () {
-  this.res.writeHead(200, { 'Content-Type': 'text/plain' });
-  this.res.end('lists');
+  this.res.writeHead(200, { 'Content-Type': 'application/json' });
+  this.res.end(JSON.stringify({lists:[]}));
+});
+
+app.router.get('/lists/:name', function (name) {
+  this.res.writeHead(404, { 'Content-Type': 'application/json' });
+  this.res.end(JSON.stringify({list:{name: name}}));
 });
 
 app.start(8080);
